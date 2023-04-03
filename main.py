a4ee53c0e79c43ffa60e98dc6dfea720be4cd1bd
michivonah <info@michivonah>
2023-04-03 18:43:53 +0200

    begin with python script #3

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import streamlit as st
+from nicegui import ui
 
-st.title('Helpdesk by Michi')
-
-st.write("""
+ui.markdown('''
 # Helpdesk
 This is a simple helpdesk tool.
-Please connect to a database.
-""")
+''')
+
+ui.link('View on GitHub', 'https://github.com/michivonah/helpdesk')
+            
+ui.button('Load tickets!', on_click=lambda: ui.notify('Please connect to a database!', close_button='OK'))
+
+ui.input(label='Username')
+
+ui.input(label='Password')
+
+ui.button('Sign in')
+
+# Start App
+ui.colors(primary='#9b59b6', secondary='#8e44ad', accent='#f1c40f')
+ui.run(title='Helpdesk by Michi', dark='false')
 
