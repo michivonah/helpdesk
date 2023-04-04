c7e1456fbce6d49c947e0f8606281f96c13809c4
michivonah <info@michivonah>
2023-04-04 21:53:26 +0200

    change the columnnames #1

--- DB/select_tickets.sql
+++ DB/select_tickets.sql
@@ -5,6 +5,6 @@ Michi von Ah - April 2023
 
 DROP VIEW IF EXISTS "alltickets";
 CREATE VIEW "alltickets" AS
-	SELECT ticketid, ticket."name", ticket."description", "customer"."name" AS "Customer", "user"."username" AS "Assigned" FROM ticket
+	SELECT ticketid AS "Ticketnumber", ticket."name" AS "Ticketname", ticket."description" AS "Description", "customer"."name" AS "Customer", "user"."username" AS "Assigned to" FROM ticket
 		JOIN customer ON customerid = fk_customerid
 		JOIN "user" ON userid = fk_userid;
