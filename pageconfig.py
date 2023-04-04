a525c9d5ef190de4b12304396cf055e343cdab0e
michivonah <info@michivonah>
2023-04-04 17:19:36 +0200

    add pageconfig #3

---
+++ pageconfig.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Helpdesk by Michi",
+    page_icon="🎟️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# Helpdesk by Michi von Ah"
+    }
+)
