9c215a1fca66ff093fa96c524a646c69f70fa718
michivonah <info@michivonah>
2023-04-04 18:05:47 +0200

    work at ticket view #3

--- pages/tickets.py
+++ pages/tickets.py
@@ -1,7 +1,23 @@
 import streamlit as st
+import psycopg2
+import pandas as pd
+import numpy as np
 
 st.write("""
 # My tickets
 """)
          
 st.sidebar.markdown("# My tickets")
+
+conn = psycopg2.connect(
+    host="helpdesk-db",
+    database="helpdesk",
+    user="helpdesk",
+    password="password",
+    port="5454",)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM alltickets")
+
+print(cursor.fetchall())
