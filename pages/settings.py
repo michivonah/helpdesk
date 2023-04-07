dc42170326e8ef199d7d0fe1e0d7b60177a9bb50
michivonah <info@michivonah>
2023-04-07 17:46:01 +0200

    fix error in settings #3

--- pages/settings.py
+++ pages/settings.py
@@ -8,14 +8,16 @@ st.write("""
 # Settings
 """)
          
+if 'loginSucceed' not in st.session_state:
+    st.session_state['loginSucceed'] = False
+         
 if st.session_state.loginSucceed:
     st.write('You are logged in')
     newUsername = st.text_input('Username', st.session_state.username)
     saveBtn = st.button('Save changes')
     st.info(f"Your UserID: {int(st.session_state.userid)}")
+    if saveBtn:
+        dbfunctions.executeQuery(f"UPDATE \"user\" SET username = '{newUsername}' WHERE userid = {int(st.session_state.userid)};")
+        st.success('Username changed', icon="✅")
 else:
     st.write('You are not logged in. Please log in before accessing the settings.')
-
-if saveBtn:
-    dbfunctions.executeQuery(f"UPDATE \"user\" SET username = '{newUsername}' WHERE userid = {int(st.session_state.userid)};")
-    st.success('Username changed', icon="✅")
