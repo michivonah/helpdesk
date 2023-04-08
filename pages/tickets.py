40555a646f46475019cb0e51bad1b59d6f73b6a9
michivonah <info@michivonah>
2023-04-08 16:32:11 +0200

    update ticket list #3

--- pages/tickets.py
+++ pages/tickets.py
@@ -9,19 +9,41 @@ def createTicket(name, desc, customer, assign):
     customerid = dbfunctions.executeQuery(f"SELECT DISTINCT customerid, name from \"customer\" WHERE name = '{customer}';")[0][0]
     userid = dbfunctions.executeQuery(f"SELECT DISTINCT userid, username from \"user\" WHERE username = '{assign}';")[0][0]
     dbfunctions.executeWithoutFetch(f"INSERT INTO ticket (name, description, fk_statusid, fk_userid, fk_customerid) VALUES ('{name}', '{desc}', 1, {userid}, {customerid});")
+    st.success("Ticket created", icon="✅")
+
+def loadTicketlist(closed, orderBy):
+    if closed:
+        st.dataframe(dbfunctions.loadTable(f"SELECT * FROM alltickets ORDER BY \"{orderBy}\""), use_container_width=True)
+    else:
+        st.dataframe(dbfunctions.loadTable(f"SELECT * FROM alltickets WHERE \"Status\" = 'Open' ORDER BY \"{orderBy}\""), use_container_width=True)
+
+def getCustomerList():
+    customerList = dbfunctions.executeQuery(f"SELECT \"name\" FROM customer;")
+    customers = ()
+    for customer in customerList:
+        customers = customers + (customer[0],)
+    return customers
 
 st.write("""
 # Tickets
 """)
+         
+if 'username' not in st.session_state:
+    st.session_state['username'] = "SYSTEM"
 
 ticketList, myTickets, newTicket = st.tabs(["All Tickets", "My Tickets", "Create new ticket"])
 
 with ticketList:
-    showClosedTickets = st.checkbox('Show completed tickets')
-    if showClosedTickets:
-        st.dataframe(dbfunctions.loadTable("SELECT * FROM alltickets ORDER BY \"Ticketnumber\""), use_container_width=True)
+    toggleClose, toggleCustomer = st.columns(2)
+    showClosedTickets = toggleClose.checkbox('Show completed tickets')
+    orderByName = toggleClose.checkbox('Order by name')
+    selectedCustomers = toggleCustomer.multiselect(
+    'Choose customers to show',
+    getCustomerList())
+    if orderByName:
+        loadTicketlist(showClosedTickets, 'Ticketname')
     else:
-        st.dataframe(dbfunctions.loadTable("SELECT * FROM alltickets WHERE \"Status\" = 'Open' ORDER BY \"Ticketnumber\""), use_container_width=True)
+        loadTicketlist(showClosedTickets, 'Ticketnumber')
 
 with myTickets:
     st.dataframe(dbfunctions.loadTable(f"SELECT * FROM alltickets WHERE \"Status\" = 'Open' AND \"Assigned to\" = '{st.session_state.username}' ORDER BY \"Ticketnumber\""), use_container_width=True)
@@ -29,10 +51,7 @@ with myTickets:
 with newTicket:
     with st.container():
         newTicketname = st.text_input('Ticketname')
-        customerList = dbfunctions.executeQuery(f"SELECT \"name\" FROM customer;")
-        customers = ()
-        for customer in customerList:
-            customers = customers + (customer[0],)
+        customers = getCustomerList()
         userList = dbfunctions.executeQuery(f"SELECT username FROM \"user\";")
         users = ()
         for user in userList:
