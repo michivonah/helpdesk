ff8f2e1af31ba2a5fbc21bc1828dd7420e449265
michivonah <info@michivonah>
2023-05-07 13:00:18 +0200

    add ability to update ticket & close it #3

--- pages/tickets.py
+++ pages/tickets.py
@@ -1,31 +1,31 @@
 import streamlit as st
-import dbfunctions
+import dbfunctions as db
 import branding
          
 branding.loadBranding()
 
 # create a new ticket
 def createTicket(name, desc, customer, assign):
-    customerid = dbfunctions.executeQuery(f"SELECT DISTINCT customerid, name from \"customer\" WHERE name = '{customer}';")[0][0]
-    userid = dbfunctions.executeQuery(f"SELECT DISTINCT userid, username from \"user\" WHERE username = '{assign}';")[0][0]
-    dbfunctions.executeWithoutFetch(f"INSERT INTO ticket (name, description, fk_statusid, fk_userid, fk_customerid) VALUES ('{name}', '{desc}', 1, {userid}, {customerid});")
+    customerid = db.executeQuery(f"SELECT DISTINCT customerid, name from \"customer\" WHERE name = '{customer}';")[0][0]
+    userid = db.executeQuery(f"SELECT DISTINCT userid, username from \"user\" WHERE username = '{assign}';")[0][0]
+    db.executeWithoutFetch(f"INSERT INTO ticket (name, description, fk_statusid, fk_userid, fk_customerid) VALUES ('{name}', '{desc}', 1, {userid}, {customerid});")
     st.success("Ticket created", icon="✅")
 
 def loadTicketlist(closed, orderBy):
     if closed:
-        st.dataframe(dbfunctions.loadTable(f"SELECT * FROM alltickets ORDER BY \"{orderBy}\""), use_container_width=True)
+        st.dataframe(db.loadTable(f"SELECT * FROM alltickets ORDER BY \"{orderBy}\""), use_container_width=True)
     else:
-        st.dataframe(dbfunctions.loadTable(f"SELECT * FROM alltickets WHERE \"Status\" = 'Open' ORDER BY \"{orderBy}\""), use_container_width=True)
+        st.dataframe(db.loadTable(f"SELECT * FROM alltickets WHERE \"Status\" = 'Open' ORDER BY \"{orderBy}\""), use_container_width=True)
 
 def getSelectableList(field, table):
-    listQuery = dbfunctions.executeQuery(f'SELECT "{field}" FROM {table} ORDER BY {field};')
+    listQuery = db.executeQuery(f'SELECT "{field}" FROM {table} ORDER BY {field};')
     list = ()
     for listItem in listQuery:
         list = list + (listItem[0],)
     return list
 
 def getSelectableTicketList(query):
-    listQuery = dbfunctions.executeQuery(query)
+    listQuery = db.executeQuery(query)
     list = ()
     for listItem in listQuery:
         list = list + (f"{listItem[0]} - {listItem[1]}",)
@@ -34,23 +34,28 @@ def getSelectableTicketList(query):
 def openTicket(ticketid):
     st.write(f"### Ticket Nr. {ticketid}")
     # get data
-    ticketInfo = dbfunctions.executeQuery(f'SELECT * FROM ticket WHERE ticketid = {ticketid}')
+    ticketInfo = db.executeQuery(f'SELECT * FROM ticket WHERE ticketid = {ticketid}')
     # build page
     st.markdown(ticketInfo[0][2])
     ticketIdField, ticketNameField = st.columns(2)
     ticketIdField.text_input('Ticket Nr', ticketid, disabled=True)
-    ticketNameField.text_input('Ticketname', ticketInfo[0][1], disabled=True)
-    st.text_area('Description', ticketInfo[0][2], disabled=True)
+    ticketName = ticketNameField.text_input('Ticketname', ticketInfo[0][1])
+    ticketDescription = st.text_area('Description', ticketInfo[0][2])
     if int(ticketInfo[0][3]) == 1:
         ticketClosed = st.checkbox('Ticket closed', False)
     else:
         ticketClosed = st.checkbox('Ticket closed', True)
     saveBtn = st.button('Save changes')
     if saveBtn:
-        updateTicket()
+        updateTicket(ticketid, ticketName, ticketDescription, ticketClosed)
 
-def updateTicket():
-    st.warning('Ticket not modified', icon="⚠️")
+def updateTicket(ticketid, name, desc, closed):
+    if closed:
+        closedName = "Closed"
+    else:
+        closedName = "Open"
+    db.executeWithoutFetch(f"UPDATE ticket SET name = '{name}', description = '{desc}', \"fk_statusid\" = (SELECT DISTINCT statusid FROM status WHERE \"name\" = '{closedName}')  WHERE ticketid = {ticketid};")
+    st.success("Ticket modified", icon="✅")
     
 
 st.write("""
@@ -73,15 +78,16 @@ with ticketList:
         loadTicketlist(showClosedTickets, 'Ticketname')
     else:
         loadTicketlist(showClosedTickets, 'Ticketnumber')
+    st.write("Press **R** for refreshing the ticket list")
 
 with myTickets:
-    st.dataframe(dbfunctions.loadTable(f"SELECT * FROM alltickets WHERE \"Status\" = 'Open' AND \"Assigned to\" = '{st.session_state.username}' ORDER BY \"Ticketnumber\""), use_container_width=True)
+    st.dataframe(db.loadTable(f"SELECT * FROM alltickets WHERE \"Status\" = 'Open' AND \"Assigned to\" = '{st.session_state.username}' ORDER BY \"Ticketnumber\""), use_container_width=True)
 
 with newTicket:
     with st.container():
         newTicketname = st.text_input('Ticketname')
         customers = getSelectableList('name', 'customer')
-        userList = dbfunctions.executeQuery(f"SELECT username FROM \"user\";")
+        userList = db.executeQuery(f"SELECT username FROM \"user\";")
         users = ()
         for user in userList:
             users = users + (user[0],)
