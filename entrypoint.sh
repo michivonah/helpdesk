5905f87e23e1e6dcc31cc96ad7cf2854e13d8347
michivonah <info@michivonah>
2023-05-15 20:29:02 +0200

    add script execution to dockerimage #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,15 @@ echo 'DBNAME = "'"$DBNAME"'"' >> .streamlit/secrets.toml
 echo 'DBUSER = "'"$DBUSER"'"' >> .streamlit/secrets.toml
 echo 'DBPASSWORD = "'"$DBPASSWORD"'"' >> .streamlit/secrets.toml
 
+# Install psql client
+apt update -y
+apt-get install postgresql-client -y
+
+# Run scripts for creating db structure
+echo '"'"$DBHOST"'":"'"$DBPORT"'":"'"$DBNAME"'":"'"$DBUSER"'":"'"$DBPASSWORD"'"' > /root/.pgpass
+chmod 600 /root/.pgpass
+psql -h $DBHOST -p $DBPORT -U $DBUSER -d $DBNAME -f "DB/create_database.sql"
+psql -h $DBHOST -p $DBPORT -U $DBUSER -d $DBNAME -f "DB/create_views.sql"
+
 # Run app
 python3 -m streamlit run main.py
