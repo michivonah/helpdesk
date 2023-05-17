68590b33b9ef1c4e4425d3348620ec89d6ebede8
michivonah <info@michivonah>
2023-05-17 22:09:11 +0200

    update db & scripts #1

--- DB/create_sample_data.sql
+++ DB/create_sample_data.sql
@@ -3,19 +3,29 @@ CREATE SAMPLE DATA
 Michi von Ah - April 2023
 */
 
-INSERT INTO "user" (username, password, mail) VALUES
-    ('fritz', 'fritz', 'fritz@example.com'),
-    ('max', 'max', 'max@example.com');
+INSERT INTO "user" (username, displayname, password, mail) VALUES
+    ('fritz', 'Fritz Müller', 'c40727c66d04b725818d4579040bcc69d5cfd9b430889a1af92f02e2cdb1bde2', 'fritz@example.com'),
+    ('max', 'Max Mustermann', '9baf3a40312f39849f46dad1040f2f039f1cffa1238c41e9db675315cfad39b6', 'max@example.com');
 
-INSERT INTO "customer" (name) VALUES
-    ('ABC Buchstaben GmbH'),
-    ('XYZ Firma'),
-    ('Datenbanken Inc.'),
-    ('Docker4Life AG');
+INSERT INTO "organization" (name, domain, fk_userid) VALUES
+    ('mangoTicket Default', 'michivonah-ticket.streamlit.app', 1),
+    ('mangoTicket Demo Version', 'ticket.mchvnh.ch', 1);
 
-INSERT INTO "ticket" (name, fk_statusid, fk_userid, fk_customerid) VALUES
-    ('Drucker funktioniert nicht', 1, 1, 4),
-    ('Neues Smartphone', 1, 1, 2),
-    ('Notebook für neue Mitarbeiterin', 1, 2, 3),
-    ('Arbeitsplatz bereitstellen', 1, 2, 3);
+INSERT INTO "userorganization" (fk_userid, fk_organizationid) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 1),
+    (4, 2);
+
+INSERT INTO "customer" (name, fk_organizationid) VALUES
+    ('ABC Buchstaben GmbH', 1),
+    ('XYZ Firma', 1),
+    ('Datenbanken Inc.', 2),
+    ('Docker4Life AG', 2);
+
+INSERT INTO "ticket" (name, fk_statusid, fk_userid, fk_customerid, fk_organizationid) VALUES
+    ('Drucker funktioniert nicht', 1, 1, 4, 1),
+    ('Neues Smartphone', 1, 1, 2, 1),
+    ('Notebook für neue Mitarbeiterin', 1, 2, 3, 2),
+    ('Arbeitsplatz bereitstellen', 1, 2, 3, 2);
 
---
+++ pages/users.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import dbfunctions
+import branding
+import usermanagement as usr
+         
+branding.loadBranding()
+
+st.write("""
+# User and organization management
+""")
