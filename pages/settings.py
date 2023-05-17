d08a10f1d45293f004af570a457fe798d91b2870
michivonah <info@michivonah>
2023-05-17 22:30:19 +0200

    changes to settings page #3

--- pages/settings.py
+++ pages/settings.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import dbfunctions
+import dbfunctions as db
 import branding
-import usermanagement as usr # For detection if user is logged in
+import usermanagement as usr
          
 branding.loadBranding()
 
@@ -12,10 +12,11 @@ st.write("""
 if usr.checkLogin():
     st.write('You are logged in')
     newUsername = st.text_input('Username', st.session_state.username)
+    newDisplayname = st.text_input('Displayname', db.executeQuery(f"SELECT displayname FROM \"user\" WHERE userid = {st.session_state.userid}")[0][0])
     saveBtn = st.button('Save changes')
     st.info(f"Your UserID: {int(st.session_state.userid)}")
     if saveBtn:
-        dbfunctions.executeWithoutFetch(f"UPDATE \"user\" SET username = '{newUsername}' WHERE userid = {int(st.session_state.userid)};")
+        db.executeWithoutFetch(f"UPDATE \"user\" SET username = '{newUsername}' WHERE userid = {int(st.session_state.userid)};")
         st.success('Username changed', icon="✅")
 else:
     usr.showError()
